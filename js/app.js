26809cafd44bd4e5462dd5456230a08898e9bea4
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-21 15:20:34 +0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+        let typed = new Typed('#main-typedJS', {
+          strings: [
+            'Hello World'
+        ],
+            typeSpeed: 100,  
+            startDelay: 500,
+            backSpeed: 50,
+            backDelay: 1000,
+            loop: true,
+            loopCount: Infinity,
+        });
